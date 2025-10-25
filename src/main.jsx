e521d62412fe6d82b8f9e11bb248364a8f0043e6
meli51 <mabiabdu51@gmail.com>
2025-10-25 23:25:56 +0300

    edit2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AdminAuthProvider } from './context/AdminAuthContex';
+import { AdminAuthProvider } from './context/AdminAuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
